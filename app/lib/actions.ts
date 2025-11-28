195f191155990a1dc763c032a8b85ae1121b80b8
anandara7891 <ananda@example.com>
2025-11-28 19:20:54 +0700

    add login link url

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,7 +2,8 @@
 
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
-import { redirect } from 'next/navigation'; // <-- Import fungsi redirect
+import { redirect } from 'next/navigation';
+import { signOut } from '@/auth';
 
 export async function authenticate(
   prevState: string | undefined,
@@ -12,7 +13,6 @@ export async function authenticate(
     // Panggil signIn, Auth.js akan otomatis mencari 'credentials' provider 
     // dan menjalankan logika di file auth.ts Anda.
     await signIn('credentials', formData); 
-
     // Jika signIn berhasil tanpa error, redirect user ke dashboard.
     // Catatan: NextAuth v5/Auth.js handles the redirect internally 
     // when using credentials, but explicit redirect is sometimes safer.
@@ -29,7 +29,13 @@ export async function authenticate(
           return 'Terjadi kesalahan yang tidak terduga.';
       }
     }
+
     // Penting: Throw error selain AuthError untuk penanganan error Next.js yang lebih baik
     throw error;
   }
+}
+
+export async function logout() {
+    await signOut();
+    redirect('/login');
 }
