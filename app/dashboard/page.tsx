82cb5f70e203fdbae6eb3e2d1f2f20d20c6b71c4
anandara7891 <ananda@example.com>
2026-01-24 06:03:49 +0700

    fix: update 251225

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,17 @@
-export default function page() {
-  return <p>Dashboard Page</p>;
+import SideNav from "@/app/ui/dashboard/sidenav";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      {/* Bagian Sidebar */}
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+
+      {/* Bagian Konten Utama */}
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+        {children}
+      </div>
+    </div>
+  );
 }
