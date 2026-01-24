a0006707f404b89a4beca8dcee9f280d154048d1
anandara7891 <ananda@example.com>
2026-01-24 09:32:40 +0700

    fix : page dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,10 +3,15 @@ import SideNav from "@/app/ui/dashboard/sidenav";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      {/* Bagian Sidebar */}
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+
+      {/* Bagian Konten Utama */}
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+        {children}
+      </div>
     </div>
   );
 }
